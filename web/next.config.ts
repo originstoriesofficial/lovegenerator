7e4bc0c644b2c24020ac6b1eb5900f6c604b6167
sneha gupta <snehagupta98930@gmail.com>
2025-02-13 23:27:53 +0530

    update

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "90mb",
+    },
+    // this has to be there in order to build the project properly
+    serverComponentsExternalPackages: ["@aws-sdk"],
+  }
 };
 
 export default nextConfig;
